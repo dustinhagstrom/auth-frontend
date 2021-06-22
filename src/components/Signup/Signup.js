e795fdecfd1369d97bfeda2c876d75329b5e35bd
dustnhgstrm <77700279+dustnhgstrm@users.noreply.github.com>
2021-06-22 10:32:47 -0400

    set up logic to handle leaving input fields blank

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,35 +1,171 @@
 import React, { Component } from "react";
 import "./Signup.css";
-
 export class Signup extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    firstNameError: "",
+    lastNameError: "",
+    usernameError: "",
+    emailError: "",
+    passwordError: "",
+    confirmPasswordError: "",
+  };
+
+  handleOnChange = (event) => {
+    this.setState(
+      {
+        [event.target.name]: event.target.value,
+      },
+      () => {
+        // console.log("Inside setState Callback");
+        // console.log(this.state.firstName);
+        // console.log(event.target.name, ":", event.target.value);
+      }
+    );
+  };
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(this.state);
+  };
+
+  handleOnBlur = (event) => {
+    if (this.state[event.target.name].length === 0) {
+      this.setState({
+        [`${event.target.name}Error`]: `${event.target.placeholder} cannot be empty`,
+      });
+    }
+  };
+
   render() {
+    const {
+      firstName,
+      lastName,
+      username,
+      email,
+      password,
+      confirmPassword,
+      firstNameError,
+      lastNameError,
+      usernameError,
+      emailError,
+      passwordError,
+      confirmPasswordError,
+    } = this.state;
+
     return (
       <div className="container">
         <div className="form-text">Sign up</div>
         <div className="form-div">
-          <form className="form">
+          <form className="form" onSubmit={this.handleOnSubmit}>
             <div className="form-group-inline">
               <div className="inline-container">
                 <label htmlFor="firstName">First Name</label>
-                <input id="firstName" />
+                <input
+                  type="text"
+                  id="firstName"
+                  value={firstName}
+                  placeholder="First Name"
+                  name="firstName"
+                  onChange={this.handleOnChange}
+                  autoFocus
+                  onBlur={this.handleOnBlur}
+                />
+                <div className="errorMessage">
+                  {firstNameError && firstNameError}
+                  {/* if this is a truthy value then return the firstNameError */}
+                </div>
               </div>
               <div className="inline-container">
                 <label htmlFor="lastName">Last Name</label>
-                <input id="lastName" />
+                <input
+                  type="text"
+                  id="lastName"
+                  value={lastName}
+                  placeholder="Last Name"
+                  name="lastName"
+                  onChange={this.handleOnChange}
+                  onBlur={this.handleOnBlur}
+                />
+                <div className="errorMessage">
+                  {lastNameError && lastNameError}
+                </div>
               </div>
             </div>
-
             <div className="form-group-block">
               <div className="block-container">
                 <label htmlFor="email">Email</label>
-                <input type="text" id="email" />
+                <input
+                  type="text"
+                  id="email"
+                  value={email}
+                  placeholder="Email"
+                  name="email"
+                  onChange={this.handleOnChange}
+                  onBlur={this.handleOnBlur}
+                />
+                <div className="errorMessage">{emailError && emailError}</div>
               </div>
             </div>
             <div className="form-group-block">
               <div className="block-container">
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" />
+                <input
+                  type="text"
+                  id="username"
+                  value={username}
+                  placeholder="username"
+                  name="username"
+                  onChange={this.handleOnChange}
+                  onBlur={this.handleOnBlur}
+                />
+                <div className="errorMessage">
+                  {usernameError && usernameError}
+                </div>
               </div>
+            </div>
+            <div className="form-group-block">
+              <div className="block-container">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="text"
+                  id="password"
+                  value={password}
+                  placeholder="Password"
+                  name="password"
+                  onChange={this.handleOnChange}
+                  onBlur={this.handleOnBlur}
+                />
+                <div className="errorMessage">
+                  {passwordError && passwordError}
+                </div>
+              </div>
+            </div>
+            <div className="form-group-block">
+              <div className="block-container">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input
+                  type="text"
+                  id="confirmPassword"
+                  value={confirmPassword}
+                  placeholder="Confirm Password"
+                  name="confirmPassword"
+                  onChange={this.handleOnChange}
+                  onBlur={this.handleOnBlur}
+                />
+                <div className="errorMessage">
+                  {confirmPasswordError && confirmPasswordError}
+                </div>
+              </div>
+            </div>
+            <div className="button-container">
+              <button>Submit</button>
             </div>
           </form>
         </div>
@@ -37,5 +173,4 @@ export class Signup extends Component {
     );
   }
 }
-
 export default Signup;
